115e92b3720fd232792af352b5fb4ece50b29abe
dylan lewis <djl51@kent.ac.uk>
2026-02-09 12:04:25 +0000

    v2

--- part_1.py
+++ part_1.py
@@ -1,4 +1,4 @@
-#task 1 part 1 
+#task1 part1 
 #make the list 
 
 sample_bay = ["Basalt", "Silica", "Iron", "Dust"]
@@ -6,3 +6,10 @@ sample_bay = ["Basalt", "Silica", "Iron", "Dust"]
 print(f"first_sample {sample_bay[0]}")
 print(f"last_sample {sample_bay[-1]}")
 print(f"sample_total: {len(sample_bay)}")
+
+#task1 part2
+#write code to transmitt data from rover to hq
+
+for i in range(4):
+    print(f"Transmitting data for: [{sample_bay[i]}]")
+
