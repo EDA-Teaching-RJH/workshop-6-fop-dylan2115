56529237eaef63895359271f51d78540c50f980f
dylan lewis <djl51@kent.ac.uk>
2026-02-09 11:38:46 +0000

    v1

--- part_1.py
+++ part_1.py
@@ -1,1 +1,8 @@
+#task 1 part 1 
+#make the list 
 
+sample_bay = ["Basalt", "Silica", "Iron", "Dust"]
+
+print(f"first_sample {sample_bay[0]}")
+print(f"last_sample {sample_bay[-1]}")
+print(f"sample_total: {len(sample_bay)}")
